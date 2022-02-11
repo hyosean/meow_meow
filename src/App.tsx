6ff5c1c0634e5491b9bf51a7a186550852a653f8
HyoSean <kimhyosun@gmail.com>
2022-02-11 16:38:32 +0900

    타입 지정 및 엔터시 검색 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, KeyboardEvent} from 'react';
 import './App.css';
 import config from './config.json';
 import axios from 'axios';
 
-function App() {
-	const [catList, setCatList] = useState([]);
-	const [targetValue, setTargetValue] = useState('');
-	const [targetCatList, setTargetCatList] = useState([]);
+function App(): JSX.Element {
+	interface ListItem {
+		name: string;
+		image: {url: string};
+		country_codes: string;
+		life_span: string;
+		description: string;
+	}
+	//interface TargetItem {}
+	const [catList, setCatList] = useState<ListItem[]>([]);
+	const [targetCatList, setTargetCatList] = useState<ListItem[]>([]);
+	const [targetValue, setTargetValue] = useState<string>('');
 
 	useEffect(() => {
 		axios
@@ -15,20 +23,35 @@ function App() {
 				setCatList(res.data);
 			})
 			.catch((e) => console.error(e));
-	}, []);
+	}, [targetCatList]);
 
-	const handleEvent = () => {
+	const searching = () => {
+		let nameList: string[] = catList.map((cur) => cur.name);
+		nameList = nameList.filter((cur) => cur.includes(targetValue));
+		let targetCats: ListItem[] = [];
+		for (let i: number = 0; i < nameList.length; i++) {
+			let target: ListItem[] = catList.filter((cur) => cur.name === nameList[i]);
+			targetCats.push(target[0]);
+		}
+		setTargetCatList(targetCats);
+	};
+
+	const handleEnterEvent = (e: KeyboardEvent<HTMLInputElement>) => {
+		let innerText = e.target as HTMLInputElement;
+		setTargetValue(innerText.value);
+		if (e.code === 'Enter') {
+			if (targetValue === '') {
+				alert('고양이 이름을 입력해 주세요');
+			} else {
+				searching();
+			}
+		}
+	};
+	const handleClickEvent = () => {
 		if (targetValue === '') {
 			alert('고양이 이름을 입력해 주세요');
 		} else {
-			let nameList = catList.map((cur) => cur.name);
-			nameList = nameList.filter((cur) => cur.includes(targetValue));
-			let targetCats = [];
-			for (let i = 0; i < nameList.length; i++) {
-				let target = catList.filter((cur) => cur.name === nameList[i]);
-				targetCats.push(target[0]);
-			}
-			setTargetCatList(targetCats);
+			searching();
 		}
 	};
 
@@ -36,8 +59,8 @@ function App() {
 		<div className="App">
 			<div className="search_box">
 				<h1>THE CAT SEARCH</h1>
-				<input type="search" placeholder="search about the cat" onInput={(e) => setTargetValue(e.target.value)} />
-				<button onClick={handleEvent}>search</button>
+				<input type="search" placeholder="search about the cat" onKeyPress={(e) => handleEnterEvent(e)} />
+				<button onClick={handleClickEvent}>search</button>
 			</div>
 			<ul className="result_box">
 				{targetCatList.map((cur, idx) => (
