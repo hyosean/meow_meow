71b776ed61089e47410e5e506a74e7e78b3cde40
HyoSean <kimhyosun@gmail.com>
2022-02-11 17:44:11 +0900

    초기화 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState, KeyboardEvent} from 'react';
+import {useEffect, useState, KeyboardEvent, MouseEvent} from 'react';
 import './App.css';
 import config from './config.json';
 import axios from 'axios';
@@ -6,12 +6,12 @@ import axios from 'axios';
 function App(): JSX.Element {
 	interface ListItem {
 		name: string;
-		image: {url: string};
+		image?: {url?: string};
 		country_codes: string;
 		life_span: string;
 		description: string;
 	}
-	//interface TargetItem {}
+
 	const [catList, setCatList] = useState<ListItem[]>([]);
 	const [targetCatList, setTargetCatList] = useState<ListItem[]>([]);
 	const [targetValue, setTargetValue] = useState<string>('');
@@ -23,9 +23,9 @@ function App(): JSX.Element {
 				setCatList(res.data);
 			})
 			.catch((e) => console.error(e));
-	}, [targetCatList]);
+	}, []);
 
-	const searching = () => {
+	function searching() {
 		let nameList: string[] = catList.map((cur) => cur.name);
 		nameList = nameList.filter((cur) => cur.includes(targetValue));
 		let targetCats: ListItem[] = [];
@@ -34,20 +34,32 @@ function App(): JSX.Element {
 			targetCats.push(target[0]);
 		}
 		setTargetCatList(targetCats);
-	};
+		//초기화
+		setTargetValue('');
+		let text = document.querySelector('#search_bar') as Element;
+		let textContent = text.textContent as string;
+		textContent = '';
+		console.log(textContent);
+	}
 
 	const handleEnterEvent = (e: KeyboardEvent<HTMLInputElement>) => {
 		let innerText = e.target as HTMLInputElement;
+		console.log(innerText.value);
 		setTargetValue(innerText.value);
+
 		if (e.code === 'Enter') {
 			if (targetValue === '') {
 				alert('고양이 이름을 입력해 주세요');
+				return;
 			} else {
 				searching();
+				console.log(targetValue);
 			}
+		} else {
+			return;
 		}
 	};
-	const handleClickEvent = () => {
+	const handleClickEvent = (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
 		if (targetValue === '') {
 			alert('고양이 이름을 입력해 주세요');
 		} else {
@@ -59,8 +71,8 @@ function App(): JSX.Element {
 		<div className="App">
 			<div className="search_box">
 				<h1>THE CAT SEARCH</h1>
-				<input type="search" placeholder="search about the cat" onKeyPress={(e) => handleEnterEvent(e)} />
-				<button onClick={handleClickEvent}>search</button>
+				<input type="search" id="search_bar" placeholder="search about the cat" onKeyUp={(e) => handleEnterEvent(e)} />
+				<button onClick={(e) => handleClickEvent(e)}>search</button>
 			</div>
 			<ul className="result_box">
 				{targetCatList.map((cur, idx) => (
@@ -68,7 +80,7 @@ function App(): JSX.Element {
 						<dl>
 							<dt>{cur.name}</dt>
 							<dd>
-								<img src={cur.image.url} alt={cur.name} />
+								<img src={cur.image?.url} alt={cur.name} />
 							</dd>
 							<dd>country_codes : {cur.country_codes}</dd>
 							<dd>life_span : {cur.life_span}</dd>
